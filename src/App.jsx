843c9f888325dcdd7d76ce0a52ecc2ed526cca05
Rafatl35 <rafatrahmano4@gmail.com>
2024-08-25 12:35:28 +0600

    react

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <> 
+      <h1>Vite + React</h1>
+      <Person></Person>
+      <Student></Student>
+    </>
+  )
+}
+function Person(){
+  const age = 25;
+  const money = 30;
+  const person = {name: 'sakib', age: 23}
+  return <h3>I am {person.name} person with age: {age}</h3>
+}
+
+function Student(){
+  return (<div>
+    <h3>This is a student</h3>
+    <p>Name:</p>
+    <p>Age:</p>
+
+  </div>
+  )
+}
+
+export default App

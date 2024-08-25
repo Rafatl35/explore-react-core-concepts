efc1dbfa200a6deb925d8971b2bb707734d95644
Rafatl35 <rafatrahmano4@gmail.com>
2024-08-25 13:03:49 +0600

    38-4 Explore JSX and Props basics

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,21 @@ function App() {
   return (
     <> 
       <h1>Vite + React</h1>
+      <Device name="laptop" price="55"></Device>
+      <Device name="mobile" price="15"></Device>
+      <Device name="watch" price="5"></Device>
       <Person></Person>
+      <Student grade="7" score="99"></Student>
       <Student></Student>
+      <Student></Student>
+      <Developer></Developer>
     </>
   )
+}
+
+function Device(props){
+  // console.log(props)
+  return <h2>This device: {props.name} price: {props.price}</h2>
 }
 function Person(){
   const age = 25;
@@ -20,13 +31,30 @@ function Person(){
   return <h3>I am {person.name} person with age: {age}</h3>
 }
 
-function Student(){
-  return (<div>
+function Student(props){
+  console.log(props)
+  return (
+  <div className='student'>
     <h3>This is a student</h3>
     <p>Name:</p>
     <p>Age:</p>
 
   </div>
+  )
+}
+
+function Developer(){
+  const developerStyle ={
+    margin: '20px',
+    padding: '20px',
+    border: '2px solid purple',
+    borderRadius: '10px'
+  }
+  return(
+    <div style={developerStyle}>
+      <h5>Developer</h5>
+      <p>Coding:</p>
+    </div>
   )
 }
 
